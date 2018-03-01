3996d6f2e7dc7a02e046614d1e856b4c8fb76c60
JakeLandowski <jakelandowski@gmail.com>
2018-03-01 09:13:29 -0800

    Updated error reporting on index, added runtime clock for benchmarking and changed thread.html and post.html to plural

--- index.php
+++ index.php
@@ -11,14 +11,40 @@
  //                     SETUP                      //
 //================================================//
 
-error_reporting(E_ALL);
+error_reporting(E_ALL|E_STRICT);
 ini_set('display_errors', 1);
+
+    //  Start Runtime clock
+require_once "testing/runTimeStart.php";
 
 require_once 'vendor/autoload.php';
 session_start();
 
 $f3 = Base::instance();
-$f3->set('DEBUG', 3);
+
+// ~~~~ BIG ERROR REPORTING ~~~~ //
+        ini_set('display_errors', 0);
+        // Deprecated directives
+        @ini_set('magic_quotes_gpc', 0);
+        @ini_set('register_globals', 0);
+
+        // Abort on startup error
+        // Intercept errors/exceptions; PHP5.3-compatible
+
+        set_exception_handler(function($obj) use($f3)
+        {
+            $f3->error(500,$obj->getmessage(),$obj->gettrace());
+        });
+
+        set_error_handler(function($code,$text) use($f3)
+        {
+            if (error_reporting())
+            {
+                $f3->error(500,$text);
+            }
+        });
+// ~~~~ BIG ERROR REPORTING ~~~~ //
+
 
   //================================================//
  //                   PRE-ROUTE                    //
@@ -33,10 +59,20 @@ $f3->route('GET|POST /', function()
 {
     echo Template::instance()->render('views/home.html');
 });
-    // THREAD ROUTE
-$f3->route('GET /thread', function()
+    // THREADS ROUTE
+$f3->route('GET /threads', function()
 {
-    echo Template::instance()->render('views/thread.html');
+    echo Template::instance()->render('views/threads.html');
+});
+
+// POSTS ROUTE
+$f3->route('GET /posts', function()
+{
+    echo Template::instance()->render('views/posts.html');
 });
 
 $f3->run();
+
+
+    //  End Runtime clock
+require_once "testing/runTimeEnd.php";
