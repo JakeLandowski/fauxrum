017a6375efaf483ad5db852a9fd49c49959b2d11
JakeLandowski <jakelandowski@gmail.com>
2018-03-04 12:46:26 -0800

    Fixed textmap to always start at a start of text parsed

--- classes/TextMap.php
+++ classes/TextMap.php
@@ -10,11 +10,13 @@ class TextMap
 {
     private $_signature = 0;
     private $_map = [];
-    private $_order = 5; // 3 is the lowest we can go while still making sense
+    private $_order = 3; // 3 is the lowest we can go while still making sense
 
     public function parseText(&$text)
     {
+        $text .= ' ';
         $len = strlen($text);
+        $grabAmt = 0;
         $gram;
         $charAfter;
         $offset;
@@ -24,24 +26,32 @@ class TextMap
         {
             $offset = $len - 1 - $i;
 
-            if($offset < $this->_order)
+            if($offset < $grabAmt)
             {
-                $wrapAmt = $this->_order - $offset - 1;
-                $gram = substr($text, $i, $this->_order - $wrapAmt);
+                $wrapAmt = $grabAmt - $offset - 1;
+                $gram = substr($text, $i, $grabAmt - $wrapAmt);
                 $gram .= substr($text, 0, $wrapAmt);
                 $charAfter = substr($text, $wrapAmt, 1);
+
             }
             else
             {
-                $gram = substr($text, $i, $this->_order);
-                $charAfter = substr($text, $i + $this->_order, 1);
+                $gram = substr($text, $i, $grabAmt);
+                $charAfter = substr($text, $i + $grabAmt, 1);
             }
-
+            
+            
+            if($grabAmt < $this->_order) 
+            {
+                $i--;
+                $grabAmt++;
+            }
+            
             if(!array_key_exists($gram, $this->_map))
             {
                 $this->_map[$gram] = []; 
             } 
-
+            
             if(!isset($this->_map[$gram][$charAfter]))
             {
                 $this->_map[$gram][$charAfter] = 1;
@@ -55,23 +65,25 @@ class TextMap
         }
     }
 
-    
     public function generate($length=100)
     {
-        $_weighted = $this->_weighProbabilities();
+        $weighted = $this->_weighProbabilities();
 
         if(empty($this->_map)) return '';
 
-        $seed = array_rand($this->_map);
+        $seed = '';
         $gram;
         $point;
         
-        for($i = 0; $i < $length; $i++)
+        for($i = 0 - $this->_order; $i < $length; $i++)
         {
-            $gram = substr($seed, $i, $i + $this->_order);
-            if(!isset($_weighted[$gram])) { echo 'No key found' . '<br>'; return $seed; }
-            $point = rand(0, $_weighted[$gram]['max']);
-            $seed .= $this->_findFirstRange($point, $_weighted[$gram]['probs']);
+            if($i < 0) $gram = substr($seed, 0); // while seed not big enough grab all
+            else $gram = substr($seed, $i, $i + $this->_order); // grab order length and move;
+
+                if(!isset($weighted[$gram])) { echo 'No key found' . '<br>'; return $seed; }
+
+            $point = rand(0, $weighted[$gram]['max']);
+            $seed .= $this->_findFirstRange($point, $weighted[$gram]['probs']);
         }
         
         return $seed;
@@ -79,7 +91,7 @@ class TextMap
     
     private function _weighProbabilities()
     {
-        $_weighted = [];
+        $weighted = [];
         $acc;
 
         foreach($this->_map as $gram => $charSet)
@@ -89,13 +101,13 @@ class TextMap
             foreach($charSet as $char => $count)
             {
                 $acc += $count;
-                $_weighted[$gram]['probs'][$acc] = $char;
+                $weighted[$gram]['probs'][$acc] = $char;
             }
 
-            $_weighted[$gram]['max'] = $acc;
+            $weighted[$gram]['max'] = $acc;
         }
 
-        return $_weighted;
+        return $weighted;
     }
     
     
