792a9e9760642da765f763299ed7f3dfc3c304c5
JakeLandowski <jakelandowski@gmail.com>
2018-03-08 11:57:43 -0800

    Capitalized "In" in the error message to distinguish it

--- testing/CustomError.php
+++ testing/CustomError.php
@@ -5,7 +5,7 @@ abstract class CustomError
     public static final function throw($message, $level=E_USER_ERROR) 
     { 
         $caller = next(debug_backtrace()); 
-        trigger_error("$message in {$caller['function']} 
+        trigger_error("$message In {$caller['function']} 
                        called from {$caller['file']}
                        on line {$caller['line']}", $level); 
     }  
