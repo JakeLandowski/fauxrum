3907edf4e02acfab46ec7f3c42ec5cc6053a1471
JakeLandowski <jakelandowski@gmail.com>
2018-03-07 19:53:27 -0800

    Developed Condition.php a bit more, almost done with it

--- testing/Condition.php
+++ testing/Condition.php
@@ -2,12 +2,17 @@
 
 class Condition
 {
+    const VALID_TYPES =
+    [
+        'string', 'int', 'lob'
+    ];
+
     private $_expression = 
     [
-        columns     => [],
-        comparisons => [],
-        values      => [],
-        logical     => [],
+        columns      => [],
+        comparisons  => [],
+        values       => [],
+        logical      => [],
         AND_other    => [],
         OR_other     => []
     ];
@@ -36,64 +41,98 @@ class Condition
         else
         {
             $this->_numColumns++;
-            $this->_expression[$this->_state][] = trim($column);
+            $this->_expression['columns'][] = trim($column);
             $this->_state = 'comparisons';
+            // $this->_expression['logical'][] = '';
             return $this; 
         }
     }
 
-    public function equals($value)
+    public function equals($type, $value)
     {
         $this->_comparison('=');
-        $this->_val($value);
+        $this->_val($type, $value);
         return $this;
     }
 
-    public function notEquals($value)
+    public function notEquals($type, $value)
     {
         $this->_comparison('<>');
-        $this->_val($value);
+        $this->_val($type, $value);
         return $this;
     }
 
-    public function lessThan($value)
+    public function lessThan($type, $value)
     {
         $this->_comparison('<');
-        $this->_val($value);
+        $this->_val($type, $value);
         return $this;
     }
 
-    public function greaterThan($value)
+    public function greaterThan($type, $value)
     {
         $this->_comparison('>');
-        $this->_val($value);
+        $this->_val($type, $value);
         return $this;
     }
 
-    public function lessThanOrEquals($value)
+    public function lessThanOrEquals($type, $value)
     {
         $this->_comparison('<=');
-        $this->_val($value);
+        $this->_val($type, $value);
         return $this;
     }
 
-    public function greaterThanOrEquals($value)
+    public function greaterThanOrEquals($type, $value)
     {
         $this->_comparison('>=');
-        $this->_val($value);
+        $this->_val($type, $value);
         return $this;
     }
 
     public function like($value)
     {
         $this->_comparison('LIKE');
-        $this->_val($value);
+        $this->_val('string', $value);
         return $this;
     }
 
     public function getExpression()
     {
-        return $this->_expression;
+        if(!$this->isComplete())
+            CustomError::throw("Cannot resolve expression, this condition is incomplete.");
+        else 
+            return $this->_expression;
+    }
+
+    public function __toString()
+    {
+        if(!$this->isComplete()) return '';
+
+        $str = '(';
+        $count = count($this->_expression['columns']);
+
+        for($i = 0; $i < $count; $i++)
+        {
+            $str .= $this->_expression['columns'][$i] . ' ';
+            $str .= $this->_expression['comparisons'][$i] . ' ';
+            $str .= ':' . $this->_expression['values'][$i]['value'];
+            if($i > 0) $str .= $this->_expression['logical'][$i - 1];
+        }
+
+        $str .= ')'; 
+
+        foreach($this->_expression['AND_other'] as $other)
+        {
+            $str .= ' AND ' . $other;
+        }
+
+        foreach($this->_expression['OR_other'] as $other)
+        {
+            $str .= ' OR ' . $other;
+        }
+
+        return $str;
     }
 
     public function isComplete()
@@ -107,13 +146,12 @@ class Condition
     public function and($otherCondition=null)
     {
         $this->_logical($otherCondition, 'AND');
-        if(!$otherCondition) return $this;
+        return $this;
     }
 
     public function or($otherCondition=null)
     {
         $this->_logical($otherCondition, 'OR');
-        if(!$otherCondition) return $this;
     }
 
   //=========================================================//
@@ -124,16 +162,16 @@ class Condition
     {
         if($this->isComplete())
         {
-            if($otherCondition)
+            if($other)
             {
-                if(!$otherCondition->isComplete())
+                if(!$other->isComplete())
                 {
-                    CustomError::throw("Tried to $type incomplete condition: $otherCondition");
+                    CustomError::throw("Tried to $type incomplete condition: $other");
                 }
                 else
                 {
-                    $this->_numLogical++;
-                    $this->_expression["{$type}_other"][] = $otherCondition;
+                    // $this->_numLogical++;
+                    $this->_expression["{$type}_other"][] = $other;
                 }
             }
             else
@@ -158,21 +196,24 @@ class Condition
         else
         {
             $this->_numComparisons++;
-            $this->_expression[$this->_state][] = trim($comparison);
+            $this->_expression['comparisons'][] = trim($comparison);
             $this->_state = 'values';
         }
     }
 
-    private function _val($value)
+    private function _val($type, $value)
     {
         if($this->_state == 'columns') 
-            CustomError::throw("Tried to set value {$value}, expected a column.");
+            CustomError::throw("Tried to set value $type {$value}, expected a column.");
         else if($this->_state == 'comparisons') 
-            CustomError::throw("Tried to set value {$value}, expected a comparison.");
+            CustomError::throw("Tried to set value $type {$value}, expected a comparison.");
+        else if(!in_array($type, Condition::VALID_TYPES))
+            CustomError::throw("Invalid type $type given for value.");
         else
         {
             $this->_numValues++;
-            $this->_expression[$this->_state][] = trim($value);
+            $this->_expression['values'][] = ['type' => strtolower(trim($type)), 
+                                              'value' => trim($value)];
             $this->_state = 'columns';
         }
     }
