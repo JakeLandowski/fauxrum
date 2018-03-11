9005eda3728f10b06c956f11d25b308208b5f2b1
JakeLandowski <jakelandowski@gmail.com>
2018-03-10 20:03:49 -0800

    Added orderby, and limits to Select function

--- testing/Database.php
+++ testing/Database.php
@@ -4,7 +4,7 @@ require_once getenv('HOME') . '/db_configs/fauxrum_config.php';
 
 abstract class Database
 {
-    const EVERYTHING = 'everything';
+    // const EVERYTHING = 'everything';
     const ONE        = 'one';
 
     const VALID_ENTRIES = 
@@ -71,9 +71,34 @@ abstract class Database
         }
     }
 
-    public static final function SELECT($columns, $table, $condition=null, $get=Database::EVERYTHING)
+    public static final function SELECT($columns, $table, $options=[])//$condition=null, $get=Database::EVERYTHING)
     {
-        $sql = Database::_buildSelect($columns, $table, $condition);
+            //  INITIALIZE OPTIONAL PARAMS
+        $fetch        = isset($options['fetch'])      ? $options['fetch']     : null;
+        $condition    = isset($options['condition'])  ? $options['condition'] : null;
+        $order_by     = isset($options['order_by'])   ? trim($options['order_by']) : null;
+        $descending   = isset($options['descending']) && 
+                              $options['descending'] ? true : false;
+        
+        $limit_start  = isset($options['limit_start']) && 
+                        is_numeric($options['limit_start']) 
+                        ? abs($options['limit_start'])  : null;
+
+        $limit_amount = isset($options['limit_amount']) && 
+                        is_numeric($options['limit_amount']) 
+                        ? abs($options['limit_amount']) : null;  
+
+        $returnValues = ['success' => false];
+
+        $validOrderBy     = Database::_validateOrderBy($order_by, $table);
+        $validLimitStart  = isset($limit_start);
+        $validLimitAmount = isset($limit_amount);
+
+        $sql = Database::_buildSelect($columns, $table, $condition, 
+                                      $validOrderBy, $validLimitStart, 
+                                      $validLimitAmount, $descending);
+
+        echo $sql;
 
         $connection = Database::connect();
 
@@ -85,35 +110,56 @@ abstract class Database
             if(Database::_conditionGiven($condition))
                 Database::_bindConditions($statement, $condition);
 
-            $statement->execute();
+            if($validOrderBy)
+                $statement->bindValue(':order_by',  $order_by,  PDO::PARAM_STR);
+            if($validLimitAmount)
+                $statement->bindValue(':limit_amount',  $limit_amount,  PDO::PARAM_INT);
+            if($validLimitStart)
+                $statement->bindValue(':limit_start',  $limit_start,  PDO::PARAM_INT); 
 
-            if($get == Database::EVERYTHING)
-                $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
-            else if($get == Database::ONE)
-                $rows = $statement->fetch(PDO::FETCH_ASSOC);
+            if($statement->execute())
+            {
+                $returnValues['success'] = true;
+             
+                if(isset($options['FETCH']) && $options['FETCH'] == Database::ONE)
+                    $returnValues['row']  = $statement->fetch(PDO::FETCH_ASSOC);
+                else
+                    $returnValues['rows'] = $statement->fetchAll(PDO::FETCH_ASSOC);
+                
+                $returnValues['num_rows'] = $statement->rowCount();
+
+                if(isset($limit_amount))
+                {
+                    $getTotalRows = 'SELECT found_rows() AS totalRows';
+                    $returnValues['total_rows'] = $connection->query($getTotalRows)
+                                                             ->fetch(PDO::FETCH_ASSOC);
+                }
+            }
 
             Database::disconnect($connection);
-
-            return $rows;
         }
         catch(PDOException $e)
         {
             Database::disconnect($connection);
             CustomError::throw('Query failed: ' . $e->getMessage());
         }
+
+        return $returnValues;
     }
 
-    public static final function SELECT_ALL($table, $condition=null, $get=Database::EVERYTHING)
+    public static final function SELECT_ALL($table, $condition=null, $options=[])//$get=Database::EVERYTHING)
     {
-        return Database::SELECT('*', $table, $condition, $get);
+        return Database::SELECT('*', $table, $condition, $options);//$get);
     }
 
     public static final function INSERT($table, $columns, $values)
     {
+        $returnValues = ['success' => false];
+
         if(!is_array($values)) $values = [$values];
 
         $sql = Database::_buildInsert($table, $columns, $values);
-
+        
         $connection = Database::connect();
 
         try
@@ -127,24 +173,35 @@ abstract class Database
                 $statement->bindValue(':value_' . ($i + 1),  $value,  Database::PDO_PARAMS[$type]); 
             }
 
-            $returnValue = false;
-            if($statement->execute()) $returnValue = $connection->lastInsertId();
+            if($statement->execute())
+            {
+                $returnValues['id'] = $connection->lastInsertId();
+                $returnValues['success'] = true;
+            }
+
+            $returnValues['num_rows'] = $statement->rowCount();
+                
             Database::disconnect($connection);
 
-            return $returnValue; // returns false if fail, otherwise last insert ID
         }
         catch(PDOException $e)
         {
             Database::disconnect($connection);
-            CustomError::throw('Query failed: ' . $e->getMessage());
+
+            if($e->getCode() == 23000)
+                $returnValues['duplicate'] = true;
+            else 
+                CustomError::throw('Query failed: ' . $e->getMessage());
         }
+
+        return $returnValues;
     }
 
     public static final function DELETE($table, $condition)
     {
-        $sql = Database::_buildDelete($table, $condition);
+        $returnValues = ['success' => false];
 
-        echo $sql;
+        $sql = Database::_buildDelete($table, $condition);
 
         $connection = Database::connect();
 
@@ -158,28 +215,30 @@ abstract class Database
             else
                 CustomError::throw("Need to give a condition for DELETE operations.");
 
-            $returnValue = false;
-            if($statement->execute()) $returnValue = $statement->rowCount();
+            if($statement->execute())
+            {
+                $returnValues['success'] = true;
+                $returnValues['num_rows'] = $statement->rowCount();
+            } 
 
             Database::disconnect($connection);
-
-            return $returnValue; // returns false if fail, otherwise num rows affected
         }
         catch(PDOException $e)
         {
-            Database::disconnect($connection);
+            Database::disconnect($connection); 
             CustomError::throw('Query failed: ' . $e->getMessage());
         }
+
+        return $returnValues;
     }
 
     public static final function UPDATE($table, $columns, $values, $condition)
-    {
-        
+    {   
+        $returnValues = ['success' => false];
+
         $sql = Database::_buildUpdate($table, $columns, $values, $condition);
         
         $connection = Database::connect();
-        
-        echo $sql;
         
         try
         {
@@ -199,23 +258,46 @@ abstract class Database
             if(Database::_conditionGiven($condition))
                 Database::_bindConditions($statement, $condition);
 
-            $returnValue = false;
-            if($statement->execute()) $returnValue = $statement->rowCount();
+            if($statement->execute())
+            {
+                $returnValues['id'] = $connection->lastInsertId();
+                $returnValues['success'] = true;
+            }
+
+            $returnValues['num_rows'] = $statement->rowCount();
 
             Database::disconnect($connection);
-
-            return $returnValue; // returns false if fail, otherwise num rows affected
         }
         catch(PDOException $e)
         {
             Database::disconnect($connection);
-            CustomError::throw('Query failed: ' . $e->getMessage());
+
+            if($e->getCode() == 23000)
+                $returnValues['duplicate'] = true;
+            else
+                CustomError::throw('Query failed: ' . $e->getMessage());
         }
+
+        return $returnValues; 
     }
 
   //=========================================================//
  //                   PRIVATE FUNCTIONS                     //
 //=========================================================//
+
+    private static final function _validateOrderBy(&$order_by, &$table)
+    { 
+        if(isset($order_by))
+        {
+            if(array_key_exists($order_by, Database::VALID_ENTRIES[$table]))
+                return true;
+            else
+                CustomError::throw("Order by \"$order_by\" is not a 
+                                    valid column in $table.");
+        } 
+        
+        return false; 
+    }
 
     private static final function _bindConditions(&$statement, &$condition)
     {
@@ -238,12 +320,16 @@ abstract class Database
         return isset($condition) && !empty($condition) && $condition instanceof Condition;
     }
 
-    private static final function _buildSelect(&$columns, &$table, &$condition)
+    private static final function _buildSelect(&$columns, &$table, &$condition, 
+                                               &$order_by, &$limit_start, &$limit_amount,
+                                               &$descending)
     {
         $table = trim($table);
         Database::validateTable($table);
         Database::_validateCondition($condition);
 
+            //  CHECK ALL COLUMNS GIVEN ARE VALID
+            //  AND BUILD THEM INTO STRING
         if(is_array($columns))
         {
             $columns = Database::_buildColumns($columns, $table);
@@ -253,10 +339,30 @@ abstract class Database
             CustomError::throw("\"$columns\" is not a valid entry for \"$table\"", 2);
         }
         
-        $columns   = trim($columns);
+        $columns = trim($columns);
 
-        return "SELECT $columns FROM $table" . 
-                (Database::_conditionGiven($condition) ? " WHERE $condition;" : ';');
+        $sql = "SELECT " . ($limit_amount ? 'SQL_CALC_FOUND_ROWS' : '') 
+                . "$columns FROM $table";
+
+            //  CHECK AND APPEND CONDITION
+        if(Database::_conditionGiven($condition))
+            $sql .= " WHERE $condition";
+
+            // CHECK AND APPEND ORDER BY
+        if($order_by)
+        {
+            $sql .= " ORDER BY :order_by";
+            if($descending) $sql .= ' DESC';
+        }
+
+            //  CHECK AND APPEND LIMITS
+        if($limit_amount)
+        {
+            $sql .= " LIMIT :limit_amount";
+            if($limit_start) $sql .= " OFFSET :limit_start";
+        }
+
+        return $sql .= ';';
     }
 
     private static final function _buildInsert(&$table, &$columns, &$values)
