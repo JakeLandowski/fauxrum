d5ebd0dd72402b4d30b8991d1dedb062d9e84fa1
JakeLandowski <jakelandowski@gmail.com>
2018-03-13 09:30:11 -0700

    Started validation of new thread in thread class more work to be done

--- model/classes/Thread.php
+++ model/classes/Thread.php
@@ -1,6 +1,8 @@
 <?php
 /**
+ *  Class used to represent a thread
  *   
+ *  DataCore => Validator => Registration
  */
 
 /**
@@ -8,7 +10,7 @@
  *  
  *  @author Jacob Landowski
  */
-class Thread extends DataCore 
+class Thread extends Validator 
 {
     protected $data = 
     [
@@ -33,8 +35,39 @@ class Thread extends DataCore
  //                   PUBLIC FUNCTIONS                      //
 //=========================================================//
 
+    /**
+     *  Validates the creation of a thread on new-thread page..
+     *  Populates errors array with errors which can later be retrieved.
+     *  Also stores the values validated for stickiness.
+     */
+    public function validate()
+    { 
+        $this->hasValidated(); // Used to prove this object has ran validation 
 
+        $missingTitle = 'Please create a thread title';
+        $invalidTitle = 'Title must 40 characters or less and not empty';
+        $missingPost  = 'You must fill out a post to submit a thread';
+        $invalidPost  = 'Post must be atleast 3 characters long and not empty';
 
+        $this->_validateField('title', $missingTitle, $invalidTitle,
+        function($value)
+        {
+            if(!empty(trim($value)) && strlen($value) <= 40)
+            {
+                $whereThisTitle = (new Condition('Thread'))->col('title')->equals($value);
+                $result = Database::SELECT('title', 'Thread', ['condition' => $whereThisTitle]);
+
+                if($result['success'] && $result['num_rows'] > 0)
+                {
+                    $this->_errors['title'] = 'This title was already used';
+                }
+
+                return true; // skip invalidTitle message given before
+            }
+
+            return false; // apply invalidTitle message
+        });
+    }
 
   //=========================================================//
  //                   PRIVATE FUNCTIONS                     //
