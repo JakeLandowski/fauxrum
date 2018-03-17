878963dd445510b5a0e6f3265e026c35812d9d47
JakeLandowski <jakelandowski@gmail.com>
2018-03-16 23:38:37 -0700

    Finished time calculator library class

---
+++ model/classes/TimeCalculate.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ *  Class for calculating how long ago threads/posts were made in a friendly format.
+ */
+
+/**
+ *  Class for calculating how long ago threads/posts were made in a friendly format.
+ *  
+ *  @author Jacob Landowski
+ */
+abstract class TimeCalculate 
+{
+    public static function getTimeSinceCreation($dateTime)
+    {
+        $secondsSince = time() - strtotime($dateTime);
+        $minutesSince = (int) ($secondsSince / 60);
+        $hoursSince   = (int) ($minutesSince / 60);
+        $daysSince    = (int) ($hoursSince / 24);
+        $monthsSince  = (int) ($daysSince / 30.436875);
+        $yearsSince   = (int) ($monthsSince / 12);
+
+        if($yearsSince   > 0) return TimeCalculate::_buildMessage($yearsSince,   'year');
+        if($monthsSince  > 0) return TimeCalculate::_buildMessage($monthsSince,  'month');
+        if($daysSince    > 0) return TimeCalculate::_buildMessage($daysSince,    'day');
+        if($hoursSince   > 0) return TimeCalculate::_buildMessage($hoursSince,   'hour');
+        if($minutesSince > 0) return TimeCalculate::_buildMessage($minutesSince, 'minute');
+        if($secondsSince > 0) return TimeCalculate::_buildMessage($secondsSince, 'second');
+    }
+
+    private static function _buildMessage($amt, $unit)
+    {
+        return "$amt ${unit}" . ($amt == 1 ? '' : 's') . ' ago';
+    }
+}
