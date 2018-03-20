468705241545efbb448adad52ec3a6687e29e913
JakeLandowski <jakelandowski@gmail.com>
2018-03-19 21:52:32 -0700

    added javascript API

---
+++ views/js/soundSmart.js
@@ -0,0 +1,15 @@
+$(document).ready(function()
+{    
+    var apiTypes = ['trivia', 'math', 'date', 'year'];
+
+    $('#sound_smart').on('click', function(e)
+    {
+        e.preventDefault();
+        $.get('//numbersapi.com/random/' + apiTypes[(Math.floor(Math.random() * apiTypes.length))], function(data) 
+        {
+            $('#content').append(' ' + data + ' ');
+            $('#new_post').submit();    
+        });
+    });
+    
+});
